e048fe48aa114d0b06d8f7111d166b36b345f11c
marysokolova <mariasokolova2004@gmail.com>
2024-03-16 18:36:52 +0300

    Changed font size

--- heart.py
+++ heart.py
@@ -49,7 +49,7 @@ def txt():
     # Set the text color to lightgreen 
     pen.color('pink') 
     pen.write("GeeksForGeeks", font=( 
-      "Times New Roman", 12, "bold")) 
+      "Times New Roman", 14, "bold")) 
 # Draw a heart 
 heart() 
   
